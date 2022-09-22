847033b93f3be00b42b871f664727e52c37d25e4
saklainMostake <saklain15-12003@diu.edu.bd>
2022-09-22 20:29:16 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+ 
+function ExternalUsers(){
+  const [users, setUsers] = useState([])
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response => Response.json())
+    .then(data => setUsers(data))
+  } , [])
+  return(
+    <div>
+      <h1>External Users</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+function User(props){
+  return(
+    <div>
+      <h3>Name:{props.name}</h3>
+      <h3>Email:{props.email}</h3>
+    </div>
+  )
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+// function Counter (){
+//   const [count, setCount] = useState(0)
+
+//   const DecreseCount = () => {
+//     const newCount = count - 1
+//     setCount(newCount)
+//   }
+//   return (
+//     <div>
+//       <h1>Count: {count} </h1>
+//       <button onClick={DecreseCount}>Decrese</button>
+//     </div>
+//   )
+// }
+
+
+
+// function Counter(){
+//   const [count, setCount] = useState(10)
+
+//   const IncreseCount = () => {
+//     const newCount = count + 1
+//     setCount(newCount)
+//   }
+//   return(
+//     <div>
+//       <h1>Count: {count} </h1>
+//       <button onClick={IncreseCount}>Increse</button>
+//     </div>
+//   )
+// }
 export default App;
